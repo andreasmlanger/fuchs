bbf39d66f6921a2cad732df45480559aaa90053d
andreasmlanger <andreas.m.langer@gmail.com>
2024-04-28 20:09:36 +0200

    fix blood app

--- blood/views.py
+++ blood/views.py
@@ -10,6 +10,8 @@ from main.plots import format_bokeh_plot
 from main.utils import get_avatar
 from .models import Blood
 
+TOOLS = 'wheel_zoom, box_zoom, reset'
+
 
 @login_required
 def index(request):
@@ -31,29 +33,27 @@ def index(request):
     labels = ('Systolic (mm)', 'Diastolic (mm)', 'Pulse')
     y_ideal = (115, 75)
 
-    delta_x = max(x) - min(x)
-    x_start = min(x) - 0.05 * delta_x
-    x_end = max(x) + 0.05 * delta_x
-
-    p1 = figure(height=280, width=900, tools='wheel_zoom, box_zoom, reset', x_range=(x_start, x_end),
-                x_axis_type='datetime', y_axis_label='mm Hg')
-
-    for i in range(2):
-        p1.rect(x=min(x) + 0.5 * delta_x, y=y_ideal[i], width=delta_x * 1.1, height=10, fill_color='#3BB273',
-                fill_alpha=0.1, line_color=None)
-        p1.line(x, [v[i] for v in y], line_width=3, color=colors[i], alpha=0.5, legend_label=labels[i])
-        p1.scatter(x, [v[i] for v in y], size=8, color=colors[i], legend_label=labels[i])
+    p1 = figure(height=280, width=900, x_axis_type='datetime', y_axis_label='mm Hg', tools=TOOLS)
+    p2 = figure(height=180, width=900, x_axis_type='datetime', y_axis_label='Pulse', tools=TOOLS)
 
     format_bokeh_plot(p1)
-
-    p2 = figure(height=180, width=900, tools='wheel_zoom, box_zoom, reset', x_axis_type='datetime',
-                y_axis_label='Pulse')
-    p2.line(x, [v[2] for v in y], line_width=3, color=colors[2], alpha=0.5, legend_label=labels[2])
-    p2.scatter(x, [v[2] for v in y], size=8, color=colors[2], legend_label=labels[2])
     format_bokeh_plot(p2)
 
-    p = column(p1, p2)
+    if len(x) > 0:
+        delta_x = max(x) - min(x)
+        p1.x_range.start = min(x) - 0.05 * delta_x
+        p1.x_range.end = max(x) + 0.05 * delta_x
 
+        for i in range(2):
+            p1.rect(x=min(x) + 0.5 * delta_x, y=y_ideal[i], width=delta_x * 1.1, height=10, fill_color='#3BB273',
+                    fill_alpha=0.1, line_color=None)
+            p1.line(x, [v[i] for v in y], line_width=3, color=colors[i], alpha=0.5, legend_label=labels[i])
+            p1.scatter(x, [v[i] for v in y], size=8, color=colors[i], legend_label=labels[i])
+
+        p2.line(x, [v[2] for v in y], line_width=3, color=colors[2], alpha=0.5, legend_label=labels[2])
+        p2.scatter(x, [v[2] for v in y], size=8, color=colors[2], legend_label=labels[2])
+
+    p = column(p1, p2)
     p.sizing_mode = 'stretch_width'
     bokeh_html = file_html(p, CDN, 'bokeh')
 
