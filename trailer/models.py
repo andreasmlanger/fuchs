de031cff2f7ed45c946376e57ad96a24cd05d8da
Fuchs <andreas.m.langer@gmail.com>
2024-05-04 07:25:51 +0200

    increased model charfield length for img_url

--- trailer/models.py
+++ trailer/models.py
@@ -6,7 +6,7 @@ class Trailer(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='trailer')
     title = models.CharField(max_length=200)
     url = models.CharField(max_length=200)
-    img_url = models.CharField(max_length=200)
+    img_url = models.CharField(max_length=400)
     muted = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     objects = models.Manager()
