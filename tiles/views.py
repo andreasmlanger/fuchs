18cf107a4a765c0a53943df6e108f3aceb09e099
andreasmlanger <andreas.m.langer@gmail.com>
2024-05-30 11:46:37 +0200

    redone 2048

--- tiles/views.py
+++ tiles/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from main.utils import get_avatar
 
 
 def index(request):
-    return render(request, 'tiles/index.html', {'avatar': get_avatar(request)})
+    return render(request, 'tiles/index.html', {})
